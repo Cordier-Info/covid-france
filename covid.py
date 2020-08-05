45658e979ed9f00afbdf98f8fff038c0b2387e34
Prof Info <66843772+Cordier-Info@users.noreply.github.com>
2020-08-05 04:15:29 +0200

    -

--- covid.py
+++ covid.py
@@ -5,16 +5,6 @@ from dateutil.parser import parse
 
 dep = input("Numéro du département ? ")
 
-# liste noms des départements
-url2 = "https://static.data.gouv.fr/resources/departements-et-leurs-regions/20190815-175403/departements-region.csv"
-download2 = requests.get(url2)
-brut2 = download2.content.decode('utf-8')
-lignes2 = brut2.split('\n')
-for li in lignes2 :
-    ch = li.split(',')
-    if ch[0]==str(dep) :
-        nom_dep = ch[1]
-
 # données gouvernementales covid
 url = "https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7"
 download = requests.get(url)
@@ -26,6 +16,16 @@ for ligne in lignes[1:-1:3] : # on saute l'en-tête et ne garde que les sexes 0 
     if champs[0] == '"{}"'.format(dep) :
         date.append(parse(champs[2].strip('"'))) # parse() sert à convertir la chaîne de caractères en une date
         hosp.append(int(champs[3]))
+
+# liste noms des départements
+url2 = "https://static.data.gouv.fr/resources/departements-et-leurs-regions/20190815-175403/departements-region.csv"
+download2 = requests.get(url2)
+brut2 = download2.content.decode('utf-8')
+lignes2 = brut2.split('\n')
+for li in lignes2 :
+    ch = li.split(',')
+    if ch[0]==str(dep) :
+        nom_dep = ch[1]
 
 fig, ax = plt.subplots(figsize=(15,10))
 ax.plot(date,hosp)
